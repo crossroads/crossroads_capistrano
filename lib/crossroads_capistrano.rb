4c55d4bde0a1acf080909934d1362aadd68a1cf7
Nathan B <nathan.f77@gmail.com>
2011-01-22 12:51:20 +0800

    Fixed namespace issues, fixed instance variable isolation issue

--- lib/crossroads_capistrano.rb
+++ lib/crossroads_capistrano.rb
@@ -1,9 +1,11 @@
-module Crossroads
-  module Capistrano
-    class << self  
-      def load_recipes(recipes)
-        recipes.each {|r| require File.join(File.dirname(__FILE__),'crossroads_capistrano',r)}
+module CrossroadsCapistrano
+  @@cap_config = Capistrano::Configuration.instance(:must_exist)
+  class << self  
+    def load_recipes(recipes)
+      @@cap_config.load do
+        recipes.each {|r| load File.join(File.dirname(__FILE__),'crossroads_capistrano',"#{r}.rb")}
       end
     end
   end
 end
+
