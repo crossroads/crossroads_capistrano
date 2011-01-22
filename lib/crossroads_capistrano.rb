11426ab50703423fcf1626050c7e9aaa33b965b3
Nathan B <nathan.f77@gmail.com>
2011-01-22 16:22:08 +0800

    Added ':all' option to load all available recipes

--- lib/crossroads_capistrano.rb
+++ lib/crossroads_capistrano.rb
@@ -3,7 +3,13 @@ module CrossroadsCapistrano
   class << self  
     def load_recipes(recipes)
       @@cap_config.load do
-        recipes.each {|r| load File.join(File.dirname(__FILE__),'crossroads_capistrano',"#{r}.rb")}
+        if recipes == :all
+          # Load all available recipes.
+          Dir.glob(File.join(File.dirname(__FILE__), 'crossroads_capistrano', '*.rb')).each{|f| load f}
+        else
+          # Load each specified recipe.
+          recipes.each {|r| load File.join(File.dirname(__FILE__),'crossroads_capistrano',"#{r}.rb")}
+        end
       end
     end
   end
