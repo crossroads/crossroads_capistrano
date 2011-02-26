b730097679c632725a03645b7e1d7bedd25556fb
Nathan B <nathan.f77@gmail.com>
2011-02-26 17:59:53 +0800

    Dont automatically write crontab.

--- lib/crossroads_capistrano/whenever.rb
+++ lib/crossroads_capistrano/whenever.rb
@@ -2,11 +2,10 @@
 # Updates the crontab using wheneverize
 #
 
-after "deploy:symlink", "deploy:update_crontab"
-
 namespace :deploy do
   desc "Update the crontab file"
   task :update_crontab, :roles => :db do
     run "cd #{current_path} && bundle exec whenever --update-crontab #{application}"
   end
 end
+
