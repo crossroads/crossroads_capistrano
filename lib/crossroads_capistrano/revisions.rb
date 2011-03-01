9d05ff1b7108faeecc8e3ce7b5854d00e7b746ca
Nathan B <nathan.f77@gmail.com>
2011-03-01 12:50:18 +0800

    Revisions task now works both pre and post deployment

--- lib/crossroads_capistrano/revisions.rb
+++ lib/crossroads_capistrano/revisions.rb
@@ -6,14 +6,20 @@ task :revisions, :roles => :app do
   puts "===== [ \033[1;36m#{application.capitalize} - #{stage.capitalize}\033[0m ]"
   puts "=== Deployed Revision: \033[1;32m#{current}\033[0m"
   puts "=== Previous Revision: \033[1;32m#{previous}\033[0m\n\n"
+
+  # If deployed and master are the same, show the difference between the last 2 deployments.
+  base_label, new_label, base_rev, new_rev = latest != current ? \
+                                             ["deployed", "master", current, latest] : \
+                                             ["previous", "deployed", previous, current]
+
   # Show difference between master and deployed revisions.
-  if (diff = `git log #{current}..#{latest} --oneline`) != ""
+  if (diff = `git log #{base_rev}..#{new_rev} --oneline`) != ""
     # Colorize refs
     diff.gsub!(/^([a-f0-9]+) /, "\033[1;32m\\1\033[0m - ")
     diff = "    " << diff.gsub("\n", "\n    ") << "\n"
     # Indent commit messages nicely, max 80 chars per line, line has to end with space.
     diff = diff.split("\n").map{|l|l.scan(/.{1,120}/).join("\n"<<" "*14).gsub(/([^ ]*)\n {14}/m,"\n"<<" "*14<<"\\1")}.join("\n")
-    puts "=== Difference between master revision and deployed revision:\n\n"
+    puts "=== Difference between #{base_label} revision and #{new_label} revision:\n\n"
     puts diff
   end
 end
