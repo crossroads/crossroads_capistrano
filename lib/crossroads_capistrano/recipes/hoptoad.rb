02aca14281532812b8b06fdb0f16f3b5ea0dfd32
Nathan Broadbent <nathan.f77@gmail.com>
2011-05-20 02:03:46 +0800

    Made output consistent with capistrano_colors. Bumped version

--- lib/crossroads_capistrano/recipes/hoptoad.rb
+++ lib/crossroads_capistrano/recipes/hoptoad.rb
@@ -9,6 +9,9 @@ namespace :deploy do
     require File.join(rails_root,'config','initializers','hoptoad')
     require 'hoptoad_tasks'
 
+    # Format HoptoadTasks output nicely.
+    HoptoadTasks.module_eval do; def self.puts(str); super " ** #{str}\n\n"; end; end
+
     rails_env = fetch(:hoptoad_env, fetch(:rails_env, "production"))
     local_user = ENV['USER'] || ENV['USERNAME']
 
@@ -16,15 +19,13 @@ namespace :deploy do
   * \033[0;32m== Notifying Hoptoad of Deploy\033[0m
       - \033[0;33mUser:\033[0m #{local_user}
       - \033[0;33mRails Environment:\033[0m #{rails_env}
-      - \033[0;33mRevision:\033[0m #{current_revision}
+      - \033[0;33mRevision:\033[1;37m #{current_revision[0,7]}\033[0m
       - \033[0;33mRepository:\033[0m #{repository}\n\n}
 
     HoptoadTasks.deploy(:rails_env      => rails_env,
                         :scm_revision   => current_revision,
                         :scm_repository => repository,
                         :local_username => local_user)
-
-    puts "\n    Hoptoad Notification Complete.\n\n"
   end
 end
 
--- lib/crossroads_capistrano/recipes/revisions.rb
+++ lib/crossroads_capistrano/recipes/revisions.rb
@@ -2,25 +2,26 @@ namespace :deploy do
   desc "Show currently deployed revision on server."
   task :revisions, :roles => :app do
     current, previous, latest = current_revision[0,7], previous_revision[0,7], real_revision[0,7]
-    puts "\n" << "-"*63
-    puts "===== Master Revision: \033[1;33m#{latest}\033[0m\n\n"
-    puts "===== [ \033[1;36m#{application} - #{stage}\033[0m ]"
-    puts "=== Deployed Revision: \033[1;32m#{current}\033[0m"
-    puts "=== Previous Revision: \033[1;32m#{previous}\033[0m\n\n"
+    branch_indent = " "*(i=10-branch.size;i<0 ? 0 : i) << branch.capitalize
+    current_is_deployed = current == latest
+    puts "\n  * \033[0;32m== Showing revisions and diffs for [\033[1;32m#{application} #{stage}\033[0;32m]\033[0m\n\n"
+    puts "        \033[1;33m#{branch_indent} Branch: \033[1;37m#{latest}\033[0m"
+    puts "        \033[#{current == latest ? 1 : 0};33mDeployed Revision: \033[#{current == latest ? 1 : 0};37m#{current}\033[0m"
+    puts "        \033[#{previous == latest ? 1 : 0};33mPrevious Revision: \033[#{previous == latest ? 1 : 0};37m#{previous}\033[0m\n\n"
 
     # If deployed and master are the same, show the difference between the last 2 deployments.
     base_label, new_label, base_rev, new_rev = latest != current ? \
-                                               ["deployed", "master", current, latest] : \
-                                               ["previous", "deployed", previous, current]
+                                               ["deployed revision", "#{branch} branch", current, latest] : \
+                                               ["previous revision", "deployed revision", previous, current]
 
     # Show difference between master and deployed revisions.
     if (diff = `git log #{base_rev}..#{new_rev} --oneline`) != ""
       # Colorize refs
-      diff.gsub!(/^([a-f0-9]+) /, "\033[1;32m\\1\033[0m - ")
-      diff = "    " << diff.gsub("\n", "\n    ") << "\n"
+      diff.gsub!(/^([a-f0-9]+) /, "\033[1;37m\\1\033[0m: ")
+      diff = " "*18 << diff.gsub("\n", "\n    ") << "\n"
       # Indent commit messages nicely, max 80 chars per line, line has to end with space.
-      diff = diff.split("\n").map{|l|l.scan(/.{1,120}/).join("\n"<<" "*14).gsub(/([^ ]*)\n {14}/m,"\n"<<" "*14<<"\\1")}.join("\n")
-      puts "=== Difference between #{base_label} revision and #{new_label} revision:\n\n"
+      diff = diff.split("\n").map{|l|l.scan(/.{1,120}/).join("\n"<<" "*28).gsub(/([^ ]*)\n {28}/m,"\n"<<" "*28<<"\\1")}.join("\n")
+      puts "  * \033[0;32m== Difference between \033[1;32m#{base_label}\033[0;32m and \033[1;32m#{new_label}\033[0;32m:\033[0m\n\n"
       puts diff
     end
   end
