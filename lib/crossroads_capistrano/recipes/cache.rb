31f13ab996bfd1cae5b9451cc81688e558b5c21e
Nathan Broadbent <nathan.f77@gmail.com>
2011-05-19 14:39:10 +0800

    Added more log tasks and cache tasks

---
+++ lib/crossroads_capistrano/recipes/cache.rb
@@ -0,0 +1,22 @@
+namespace :cache do
+  namespace :clear do
+    desc "Clear view cache and memcache but NOT page cache (images/videos). Usually called after a restart."
+    task :all do
+      view
+      memcache
+    end
+    desc "Clear memcache"
+    task :memcache, :only => { :primary => true } do
+      run "cd #{current_path} && RAILS_ENV=production rake cache:clear"
+    end
+    desc "Clear view cache (tmp/cache/) used when memcached is unavailable"
+    task :view do
+      run "cd #{current_path} && rm -rf tmp/cache/views/"
+    end
+    desc "Clear page cache (public/en/) used for images and videos"
+    task :page do
+      run "cd #{current_path} && rm -rf public/en/"
+    end
+  end
+end
+
--- lib/crossroads_capistrano/recipes/log.rb
+++ lib/crossroads_capistrano/recipes/log.rb
@@ -1,6 +1,5 @@
 namespace :log do
-
-  desc "tail production log files"
+  desc "Tail production log file"
   task :tail, :roles => :app do
     run "tail -f #{shared_path}/log/production.log" do |channel, stream, data|
       puts  # for an extra line break before the host name
@@ -8,5 +7,16 @@ namespace :log do
       break if stream == :err
     end
   end
-
+  namespace :pull do
+    desc "Pull production log file to /tmp/production.log"
+    task :production, :roles => :app do
+      run "gzip -c #{shared_path}/log/production.log > #{shared_path}/log/production.log.gz"
+      `rm -f /tmp/production.log.gz`
+      download("#{shared_path}/log/production.log.gz", "/tmp/production.log.gz", :via => :scp)
+      run "rm -f #{shared_path}/log/production.log.gz"
+      `gzip -fd /tmp/production.log.gz`
+      puts "File can be accessed at /tmp/production.log"
+    end
+  end
 end
+
