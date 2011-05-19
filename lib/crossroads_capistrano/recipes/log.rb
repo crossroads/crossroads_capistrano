ad5be5aeac6f1e32281671665f328299f0652c4c
Nathan Broadbent <nathan.f77@gmail.com>
2011-05-19 15:05:46 +0800

    Improved log access tasks

--- lib/crossroads_capistrano/recipes/log.rb
+++ lib/crossroads_capistrano/recipes/log.rb
@@ -1,18 +1,30 @@
 namespace :log do
-  desc "Tail production log file"
-  task :tail, :roles => :app do
-    run "tail -f #{shared_path}/log/production.log" do |channel, stream, data|
-      puts  # for an extra line break before the host name
-      puts "#{channel[:host]}: #{data}"
-      break if stream == :err
+  namespace :tail do
+    desc "Tail Rails production log file"
+    task :production, :roles => :app do
+      run "tail -f #{shared_path}/log/production.log" do |channel, stream, data|
+        puts "\n#{channel[:host]}: #{data}"
+        break if stream == :err
+      end
+    end
+    desc "Tail Apache access log file"
+    task :access, :roles => :app do
+      run "tail -f #{shared_path}/log/access_log" do |channel, stream, data|
+        puts "\n#{channel[:host]}: #{data}"
+        break if stream == :err
+      end
     end
   end
+
   namespace :pull do
     desc "Pull production log file to /tmp/production.log"
     task :production, :roles => :app do
       run "gzip -c #{shared_path}/log/production.log > #{shared_path}/log/production.log.gz"
       `rm -f /tmp/production.log.gz`
-      download("#{shared_path}/log/production.log.gz", "/tmp/production.log.gz", :via => :scp)
+      puts "Downloading #{shared_path}/log/production.log...\n"
+      download("#{shared_path}/log/production.log.gz", "/tmp/production.log.gz", :via => :scp)  do |channel, name, received, total|
+        print "\r   #{name}: #{(Float(received)/total*100).to_i}% complete..."
+      end
       run "rm -f #{shared_path}/log/production.log.gz"
       `gzip -fd /tmp/production.log.gz`
       puts "File can be accessed at /tmp/production.log"
