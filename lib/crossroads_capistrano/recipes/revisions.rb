a5aa4037d639b8f382c6889fee464681a50f84a6
Nathan Broadbent <nathan.f77@gmail.com>
2011-05-20 13:44:22 +0800

    Perfectionist

--- lib/crossroads_capistrano/recipes/revisions.rb
+++ lib/crossroads_capistrano/recipes/revisions.rb
@@ -6,9 +6,9 @@ namespace :deploy do
     branch_indent = " "*(i=10-branch.size;i<0 ? 0 : i) << branch.capitalize
     current_is_deployed = current == latest
     puts "\n  * \033[0;32m== Showing revisions and diffs for [\033[1;32m#{application} #{stage}\033[0;32m]\033[0m\n\n"
-    puts "        \033[1;33m#{branch_indent} Branch: \033[1;37m#{latest}\033[0m"
-    puts "        \033[#{current == latest ? 1 : 0};33mDeployed Revision: \033[#{current == latest ? 1 : 0};37m#{current}\033[0m"
-    puts "        \033[#{previous == latest ? 1 : 0};33mPrevious Revision: \033[#{previous == latest ? 1 : 0};37m#{previous}\033[0m\n\n"
+    puts "         \033[1;33m#{branch_indent} Branch: \033[1;37m#{latest}\033[0m"
+    puts "         \033[#{current == latest ? 1 : 0};33mDeployed Revision: \033[#{current == latest ? 1 : 0};37m#{current}\033[0m"
+    puts "         \033[#{previous == latest ? 1 : 0};33mPrevious Revision: \033[#{previous == latest ? 1 : 0};37m#{previous}\033[0m\n\n"
 
     # If deployed and master are the same, show the difference between the last 2 deployments.
     base_label, new_label, base_rev, new_rev = latest != current ? \
@@ -19,9 +19,9 @@ namespace :deploy do
     if (diff = `git log #{base_rev}..#{new_rev} --oneline`) != ""
       # Colorize refs
       diff.gsub!(/^([a-f0-9]+) /, "\033[1;37m\\1\033[0m: ")
-      diff = "        " << diff.gsub("\n", "\n        ") << "\n"
+      diff = "         " << diff.gsub("\n", "\n         ") << "\n"
       # Indent commit messages nicely, max 80 chars per line, line has to end with space.
-      diff = diff.split("\n").map{|l|l.scan(/.{1,120}/).join("\n"<<" "*18).gsub(/([^ ]*)\n {18}/m,"\n"<<" "*18<<"\\1")}.join("\n")
+      diff = diff.split("\n").map{|l|l.scan(/.{1,120}/).join("\n"<<" "*19).gsub(/([^ ]*)\n {19}/m,"\n"<<" "*19<<"\\1")}.join("\n")
       puts "  * \033[0;32m== Difference between \033[1;32m#{base_label}\033[0;32m and \033[1;32m#{new_label}\033[0;32m:\033[0m\n\n"
       puts diff
     end
