717ebc2840dcd0aa2969b7741cd2b75cd454135d
Nathan Broadbent <nathan.f77@gmail.com>
2011-05-26 10:59:07 +0800

    Bumped

--- lib/crossroads_capistrano/recipes/hoptoad.rb
+++ lib/crossroads_capistrano/recipes/hoptoad.rb
@@ -7,10 +7,7 @@ namespace :deploy do
     if ARGV.include?("-n")
       puts "\n ** Dry run, not notifying Hoptoad.\n\n"
     else
-      begin
-        require 'active_support/core_ext/string'
-      rescue LoadError
-      end
+      require 'active_support/core_ext/string' rescue Exception
       require 'hoptoad_notifier'
       require File.join(rails_root,'config','initializers','hoptoad')
       require 'hoptoad_tasks'
