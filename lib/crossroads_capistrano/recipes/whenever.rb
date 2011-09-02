f15004b0d06986b50f20804f94409726b20ab067
Steve Kenworthy <swkenworthy@crossroadsint.org>
2011-09-02 17:02:45 +0800

    Fixed whenever environment for live deployments.

--- lib/crossroads_capistrano/recipes/whenever.rb
+++ lib/crossroads_capistrano/recipes/whenever.rb
@@ -3,5 +3,5 @@
 #
 require 'whenever/capistrano'
 
+set :whenever_environment, defer { stage.to_s.gsub('live','production') }
 set :whenever_command, "bundle exec whenever" if fetch(:bundler, true)
-
